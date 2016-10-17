78dda352be4c6887cc19c354effb9ebb0693f011
Josh Dvir <shuky.dvir@gmail.com>
2016-10-17 20:26:05 +0100

    fixing tests

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 var exports = module.exports = {};
 
 app.get('/', function(req, res){
-  res.send('Hello');
+  res.send('Hello World');
 });
 
 var server = app.listen(3000, function(){
